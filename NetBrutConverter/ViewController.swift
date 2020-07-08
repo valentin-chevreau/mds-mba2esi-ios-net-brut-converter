608532d6ed80732b4c49257c09ab84ad35db93d6
Valentin CHEVREAU <vchevreau37@gmail.com>
2020-07-08 14:21:12 +0200

    add: project files - gross/net converter & Gitignore

---
+++ NetBrutConverter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetBrutConverter
+//
+//  Created by Valentin CHEVREAU on 08/07/2020.
+//  Copyright © 2020 MDS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
